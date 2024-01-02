79663d06a9e2789250a7675dc985fb1f157848df
Nijisha <117098569+NijishaN@users.noreply.github.com>
2024-01-02 15:17:46 +0530

    Add files via upload

---
+++ SignalRHub/Hubs/CommentsHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRHub.Hubs
+{
+    public class CommentsHub : Hub
+    {
+        public async Task SendMessage(string name, string message)
+        {
+            await Clients.All.SendAsync("ReceivedComment", name, message);
+        }
+    }
+}
